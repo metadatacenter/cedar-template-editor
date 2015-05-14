3551371471cde081c9e9661a342fc8e9a4c6838d
Aaron Straker <aaronstraker@gmail.com>
2015-05-13 17:40:47 -0700

    Updates for end of day

---
+++ app/scripts/directives/input-indicator-icons.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angularApp.directive('inputIndicatorIcons', function () {
+  return {
+    template: '<i class="cedar-svg-help-circle"></i><i class="cedar-svg-check-circle"></i><i class="cedar-svg-warning"></i>'
+  };
+});
