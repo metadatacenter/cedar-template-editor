66cf763d0b70676d77d6dbecbd9a463eb4b73ccd
Aaron Straker <aaronstraker@gmail.com>
2015-05-14 14:16:31 -0700

    More updates to runtime form page

--- app/scripts/controllers/runtime.js
+++ app/scripts/controllers/runtime.js
@@ -1,7 +1,21 @@
 'use strict';
 
-var ViewCtrl = angularApp.controller('RuntimeController', function ($rootScope, $scope, FormService, $routeParams) {
+var ViewCtrl = angularApp.controller('RuntimeController', function ($rootScope, $scope, FormService, $routeParams, $document) {
 
 	// set Page Title variable when this controller is active
 	$rootScope.pageTitle = 'Template Runtime';
+
+	$scope.init = function() {
+		// Enabling Bootstrap Popovers for help text button
+		angular.element('[data-toggle="popover"]').popover();
+
+		$document.on('click', function(e) {
+      // Check if Popovers exist and close on click anywhere but the popover toggle icon
+      if( angular.element(e.target).data('toggle') !== 'popover' && angular.element('.popover').length ) {
+      	angular.element('[data-toggle="popover"]').popover('hide');
+      }
+    });
+	};
+
+	$scope.init();
 });
--- app/scripts/directives/input-indicator-icons.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angularApp.directive('inputIndicatorIcons', function () {
-  return {
-    template: '<i class="cedar-svg-help-circle"></i><i class="cedar-svg-check-circle"></i><i class="cedar-svg-warning"></i>'
-  };
-});
