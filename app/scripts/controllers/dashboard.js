1ec054231ffd0f4cbed547500aeacda0329b9688
Aaron Straker <aaronstraker@gmail.com>
2015-06-17 16:57:20 -0700

    Dashboard sorting for favorites boolean = true, slicing down to 3 items

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -1,5 +1,20 @@
 'use strict';
 
+function sortByKey(array, key) {
+  return array.sort(function(a, b) {
+      var x = a[key]; var y = b[key];
+      return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+  });
+}
+
+function sortBoolean(array, bool) {
+  return array.sort(function(a, b) {
+    var x = a[bool],
+        y = b[bool];
+    return ((x == y) ? -1 : ((x == true) ? -1 : 1));
+  });
+}
+
 angularApp.controller('DashboardController', function ($rootScope, $scope, $http) {
 
 	// set Page Title variable when this controller is active
@@ -13,7 +28,10 @@ angularApp.controller('DashboardController', function ($rootScope, $scope, $http
   // scope array with returned list of data
   $scope.getDefaults = function(fileName, scopeArray) {
     $http.get('/static-data/dashboard/' + fileName + '.json').then(function(response) {
-      $scope[scopeArray] = response.data;
+      // Sort by the 'favorites' boolean parameter
+      var sortFavorites = sortBoolean(response.data, 'favorite');
+      // Slicing the top 3 out into new array and returning to the template
+      $scope[scopeArray] = sortFavorites.slice(0,3);
     }).catch(function(err) {
       console.log(err);
     });
