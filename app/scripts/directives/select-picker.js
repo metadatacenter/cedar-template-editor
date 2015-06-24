2fd84d048cc88e66fb57551d73fe4ffa26c76fd2
Aaron Straker <aaronstraker@gmail.com>
2015-06-24 12:04:33 -0700

    Fix some bugs around model output and default loading for selectlist, radio buttons, checkbox, date picker

--- app/scripts/directives/select-picker.js
+++ app/scripts/directives/select-picker.js
@@ -1,15 +1,33 @@
 // Enabling Bootstrap Datepickers for date inputs
 angularApp.directive('selectPicker', function ($timeout) {
   return {
-
     restrict: 'A',
+    scope: {
+      defaults: '='
+    },
     link: function ($scope, $element, attrs) {
+
+      var default_array = [];
+
+      // If default select options have been set
+      if ($scope.defaults) {
+        var default_options = $scope.defaults;
+
+        for (var property in default_options) {
+          if (default_options.hasOwnProperty(property)) {
+            // Push into array that is set via $element.selectpicker 'val' method
+            default_array.push(property);
+          }
+        }
+      }
+
       $timeout(function() {
         $element.selectpicker({
           style: 'btn-select-picker',
           iconBase: 'fa',
           tickIcon: 'fa-check',
         });
+        $element.selectpicker('val', default_array);
       }, 25);
     }
   };
