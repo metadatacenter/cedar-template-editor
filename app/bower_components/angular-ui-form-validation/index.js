de6bb86e0e3e89c628aa9d9f610f9e0e9a2b3093
Aaron Straker <aaronstraker@gmail.com>
2015-07-06 11:46:44 -0700

    Removed angular-ui-form-validation

--- app/bower_components/angular-ui-form-validation/index.js
+++
@@ -1,4 +0,0 @@
-require("./dist/angular-ui-form-validation");
-
-module.exports = angular.module('directives.customvalidation.customValidationTypes');
-//module.exports = angular.module('angular.ui.form.validation');
