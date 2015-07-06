11160c700e5ec9a497c6c6f22f252fac22f766db
Aaron Straker <aaronstraker@gmail.com>
2015-07-06 11:05:25 -0700

    Fixing some JS hint warnings in rootscope.js

--- app/scripts/rootscope.js
+++ app/scripts/rootscope.js
@@ -42,7 +42,7 @@ angularApp.run(['$rootScope',  function($rootScope) {
       return array.sort(function(a, b) {
         var x = a[bool],
             y = b[bool];
-        return ((x == y) ? -1 : ((x == true) ? -1 : 1));
+        return ((x == y) ? -1 : ((x === true) ? -1 : 1));
       });
-    }
+    };
 }]);
