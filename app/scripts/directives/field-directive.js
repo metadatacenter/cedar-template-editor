ceff6209f05315b8b4e889f291fc198f92bd563f
Marcos Martinez <marcos.martinez.romero@gmail.com>
2015-09-14 12:13:54 -0700

    Update info.required to info.required_value

--- app/scripts/directives/field-directive.js
+++ app/scripts/directives/field-directive.js
@@ -7,12 +7,12 @@ angularApp.directive('fieldDirective', function($http, $compile, $document) {
     // When form submit event is fired, check field for simple validation
     $scope.$on('submitForm', function (event) {
       // If field is required and is empty, emit failed emptyRequiredField event
-      if ($scope.field.properties.info.required && !$scope.model['value']) {
+      if ($scope.field.properties.info.required_value && !$scope.model['value']) {
         // add this field instance the the emptyRequiredField array
         $scope.$emit('emptyRequiredField', ['add', $scope.field.properties.info.title]);
       }
       // If field is required and is not empty, check to see if it needs to be removed from empty fields array
-      if ($scope.field.properties.info.required && $scope.model['value']) {
+      if ($scope.field.properties.info.required_value && $scope.model['value']) {
         //remove from emptyRequiredField array
         $scope.$emit('emptyRequiredField', ['remove', $scope.field.properties.info.title]);
       }
