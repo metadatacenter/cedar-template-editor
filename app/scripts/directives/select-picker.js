59b997ac3ba2ae33d9af07ab01d85f7d81c47af6
Marcos Martinez <marcos.martinez.romero@gmail.com>
2015-11-06 12:13:38 -0800

    Fix loading pick from a list value

--- app/scripts/directives/select-picker.js
+++ app/scripts/directives/select-picker.js
@@ -14,7 +14,7 @@ angularApp.directive('selectPicker', function ($timeout) {
       var default_array;
       if ($scope.model != undefined) {
         // If returning to an already populated select list field, load selections
-        default_array = $scope.model.value;
+        default_array = $scope.model._value;
 
       } else if ($scope.field && $scope.field.properties.info.default_option) {
         default_array = [];
