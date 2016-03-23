ad159a96c88dad70bd8e520c9dbccc62302b3e28
Dylan Vaughn <dylancvaughn@gmail.com>
2016-03-23 14:52:05 -0700

    multi-instance rendering #92

--- app/scripts/template-element/template-element.module.js
+++ app/scripts/template-element/template-element.module.js
@@ -3,16 +3,16 @@
 define([
   'angular',
   'cedar/template-editor/form/form.module',
-  'cedar/template-editor/template-element/template-element.routes',
+  'cedar/template-editor/template-element/cedar-nested-template-element.directive',
+  'cedar/template-editor/template-element/cedar-template-element.directive',
   'cedar/template-editor/template-element/create-element.controller',
-  'cedar/template-editor/template-element/element.directive',
-  'cedar/template-editor/template-element/nested-element.directive',
+  'cedar/template-editor/template-element/template-element.routes',
 ], function(angular) {
   angular.module('cedar.templateEditor.templateElement', [
     'cedar.templateEditor.form',
-    'cedar.templateEditor.templateElement.routes',
+    'cedar.templateEditor.templateElement.cedarNestedTemplateElementDirective',
+    'cedar.templateEditor.templateElement.cedarTemplateElementDirective',
     'cedar.templateEditor.templateElement.createElementController',
-    'cedar.templateEditor.templateElement.elementDirective',
-    'cedar.templateEditor.templateElement.nestedElementDirective',
+    'cedar.templateEditor.templateElement.routes',
   ]);
 });
