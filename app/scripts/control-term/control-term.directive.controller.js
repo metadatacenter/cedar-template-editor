bea1967bd94dca10b86b48919844ea935a3cb22d
Marcos Martinez <marcos.martinez.romero@gmail.com>
2016-04-18 12:21:34 -0700

    Fix bugs

--- app/scripts/control-term/control-term.directive.controller.js
+++ app/scripts/control-term/control-term.directive.controller.js
@@ -597,7 +597,7 @@ define([
 
     function assignValueConstraintToField() {
       $rootScope.schemaOf(vm.field)._valueConstraints =
-        angular.extend(vm.valueConstraint, $rootScope.schemaOf(vm.field)._valueConstraints)
+        angular.extend($rootScope.schemaOf(vm.field)._valueConstraints, vm.valueConstraint)
 
       delete vm.stageValueConstraintAction;
       vm.stagedOntologyValueConstraints = [];
