8447f74104cda56080145d4d9aeee40de4356019
Dylan Vaughn <dylancvaughn@gmail.com>
2016-05-02 23:37:43 -0700

    fix for going back in editors and passing folderId

--- app/scripts/layout/header.controller.js
+++ app/scripts/layout/header.controller.js
@@ -15,9 +15,25 @@ define([
   function HeaderController($rootScope, $location, UrlService) {
     var vm = this;
 
+    vm.goToDashboardOrBack = goToDashboardOrBack;
     vm.search = search;
     vm.searchTerm = null;
     vm.showSearch = showSearch;
+
+    function goToDashboardOrBack() {
+      var params = $location.search();
+      var path   = $location.path();
+      var url    = '/dashboard';
+      if (path != url) {
+        if (params.folderId) {
+          url += '?folderId=' + encodeURIComponent(params.folderId);
+        }
+        if (params.search) {
+          url += '?search=' + encodeURIComponent(params.search);
+        }
+      }
+      $location.url(url);
+    };
 
     function showSearch() {
       return $rootScope.showSearch;
