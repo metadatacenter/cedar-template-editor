8fda59ff8deb1e8c9f0628eebae171cb31d8de22
Marcos Martinez <marcos.martinez.romero@gmail.com>
2016-07-25 19:25:41 -0700

    Scroll to the selected class in the ontology tree

--- app/scripts/controlled-term/cedar-class-tree.directive.js
+++ app/scripts/controlled-term/cedar-class-tree.directive.js
@@ -2,58 +2,54 @@
 
 define([
   'angular'
-], function(angular) {
+], function (angular) {
   angular.module('cedar.templateEditor.controlledTerm.cedarClassTreeDirective', [])
-    .directive('cedarClassTree', cedarClassTreeDirective);
+      .directive('cedarClassTree', cedarClassTreeDirective);
 
   cedarClassTreeDirective.$inject = ['$timeout'];
 
   /**
-   * This 'cedarClassTree' directive will call the 'childTree' directive for 
+   * This 'cedarClassTree' directive will call the 'childTree' directive for
    * every child in the collection returned from the controlledTermDataService.
    */
   function cedarClassTreeDirective($timeout) {
     var directive = {
-      restrict: 'E',
-      scope: {
-        tree: '=',
-        term: '=',
-        level: '=',
-        selectedNode: '=',
-        isSelectedCallback: '=',  // why won't this work with & ?
+      restrict   : 'E',
+      scope      : {
+        tree                   : '=',
+        term                   : '=',
+        level                  : '=',
+        selectedNode           : '=',
+        isSelectedCallback     : '=',  // why won't this work with & ?
         getClassDetailsCallback: '=', // why won't this work with & ?
       },
       templateUrl: 'scripts/controlled-term/cedar-class-tree.directive.html',
-      replace: true,
-      link: linker
+      replace    : true,
+      link       : linker
     };
 
     return directive;
 
     function linker(scope, element, attrs) {
-      $timeout(function() {
+      var node = null;
+      scope.treeContainer = element.parent();
+
+      $timeout(function () {
+
         if (scope.selectedNode) {
           var id = scope.selectedNode["@id"];
-          var node = angular.element("[at_id='" + id + "']");
+          node = angular.element("[at_id='" + id + "']");
           if (node.length > 0) {
             var $container = element.parent();
-            var containerHeight = $container.height();
-            var containerWidth = $container.width();
-            var containerOffset = $container.offset();
-            var selectedNodeOffset = node.offset();
-
-            var topScrollAmount = selectedNodeOffset.top - containerOffset.top - containerHeight/2;
-            var leftScrollAmount = selectedNodeOffset.left - containerOffset.left - containerWidth/2;
-
+            var topScrollAmount = node.offset().top - $container.offset().top - $container.height() / 4;
             if (topScrollAmount > 0) {
-              $container.scrollTop(topScrollAmount);
-            }
-
-            if (leftScrollAmount > 0) {
-              $container.scrollLeft(leftScrollAmount);
+              $container.animate({
+                scrollTop: topScrollAmount
+              }, 500);
             }
           }
         }
+
       });
     }
   }
