a825efdcc0157f7d7a602b6cc1112dd0ae702797
Marcos Martinez <marcos.martinez.romero@gmail.com>
2016-08-16 11:22:06 -0700

    Remove file that was not used. Those values are read from env variables

--- tests/env.js
+++
@@ -1,5 +0,0 @@
-exports.baseUrl = 'https://cedar.metadatacenter.orgx';
-exports.testUser = 'dwillrett@stanford.edu';
-exports.testPassword = '123';
-exports.seleniumServerJar = '2.50.1-server.jar';
-exports.chromeDriver = '2.21-x64-chromedriver';
