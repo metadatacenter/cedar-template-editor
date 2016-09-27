15d70f2100573f7ff1ba624fa1cc08b715c15698
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2016-09-27 14:56:15 -0700

    Removed historical call to template server

--- app/scripts/template-element/create-element.controller.js
+++ app/scripts/template-element/create-element.controller.js
@@ -37,16 +37,6 @@ define([
     HeaderService.configure(pageId);
     StagingService.configure(pageId);
 
-    AuthorizedBackendService.doCall(
-        TemplateElementService.getAllTemplateElementsSummary(),
-        function (response) {
-          $scope.elementList = response.data;
-        },
-        function (err) {
-          UIMessageService.showBackendError('SERVER.ELEMENTS.load.error', err);
-        }
-    );
-
     $scope.primaryFieldTypes = FieldTypeService.getPrimaryFieldTypes();
     $scope.otherFieldTypes = FieldTypeService.getOtherFieldTypes();
     $scope.hideRootElement = true;
