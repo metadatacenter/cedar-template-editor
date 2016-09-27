b5add1ddacc18123020a7370cd5eb5766c226b49
martinjoconnor <martin.oconnor@stanford.edu>
2016-09-27 10:08:49 -0700

    Reverted Karma config to original

--- karma.conf.js
+++ karma.conf.js
@@ -80,18 +80,8 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true,
-
-    customLaunchers: {  
-      Chrome_travis_ci: {
-        base: 'Chrome',
-        flags: ['--no-sandbox']
-      }
-    }
+    singleRun: false
     
   });
 
-  if(process.env.TRAVIS) {  
-    config.browsers = ['Chrome_travis_ci'];
-  }
 };
