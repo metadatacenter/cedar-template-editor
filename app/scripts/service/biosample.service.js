5d7d7f1bbd1917048c1c848e2e008e901c79908a
Debra Willrett <willrett@stanford.edu>
2016-11-10 10:22:42 -0800

    forgot service

---
+++ app/scripts/service/biosample.service.js
@@ -0,0 +1,25 @@
+'use strict';
+
+define([
+  'angular'
+], function (angular) {
+  angular.module('cedar.templateEditor.service.biosampleService', [])
+      .service('BiosampleService', BiosampleService);
+
+  BiosampleService.$inject = ['HttpBuilderService', 'UrlService'];
+
+  function BiosampleService(HttpBuilderService, UrlService) {
+
+    var service = {
+      serviceId: 'BiosampleService'
+    };
+
+    service.validateInstance = function (instance) {
+      return HttpBuilderService.post(UrlService.postBiosample(), angular.toJson(instance));
+    };
+
+    return service;
+
+  };
+
+});
