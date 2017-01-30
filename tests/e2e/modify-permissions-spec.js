c81af433cd0df9ef76b91564b55b1a3459dfde6e
Rafael Goncalves <goncalvjr@gmail.com>
2017-01-30 15:12:09 -0800

    Update ownership tests. Closes metadatacenter/cedar-project#166
    Extend share modal page to allow group sharing

--- tests/e2e/modify-permissions-spec.js
+++ tests/e2e/modify-permissions-spec.js
@@ -9,8 +9,9 @@ var EC = protractor.ExpectedConditions;
 
 var testUserName1 = 'Test User 1';
 var testUserName2 = 'Test User 2';
+var everybodyGroup = 'Everybody';
 
-describe('modify-permissions', function () {
+describe('update-ownership', function () {
   var workspacePage;
   var toastyModal;
   var sweetAlertModal;
@@ -29,17 +30,17 @@ describe('modify-permissions', function () {
   });
 
   afterEach(function () {
+    browser.sleep(1000);
     workspacePage.clickLogo();
   });
 
 
   it("should give ownership of a folder owned by current user to another user", function () {
-    // create a folder
-    var folder = workspacePage.createFolder('Test');
+    var folder = workspacePage.createFolder('Owned');
 
     // change ownership of created folder to another user
     shareModal.shareResource(folder, 'folder', testUserName2, false, true);
-    browser.sleep(2000);
+    browser.sleep(1000);
     workspacePage.clickLogo();
 
     // select resource and open details sidebar
@@ -53,6 +54,65 @@ describe('modify-permissions', function () {
     expect(workspacePage.createDetailsPanelOwnerValue().getText()).toBe(testUserName2); // TODO fails due to issue #290
   });
 
+
+  it("should fail to change ownership of a folder shared as readable with current user", function () {
+    var folder = workspacePage.createFolder('Readable');
+
+    shareModal.shareResource(folder, 'folder', testUserName2, false, false);
+
+    workspacePage.logout();
+    workspacePage.login(testConfig.testUser2, testConfig.testPassword2);
+
+    workspacePage.navigateToUserFolder(testUserName1);
+    shareModal.openDialogViaMoreOptions(folder, 'folder');
+    expect(shareModal.canShare()).toBe(false);
+    shareModal.clickDone();
+  });
+
+
+  it("should fail to change ownership of a folder shared as writable with current user", function () {
+    var folder = workspacePage.createFolder('Readable');
+
+    shareModal.shareResource(folder, 'folder', testUserName1, true, false); // TODO fails due to issue #273
+
+    workspacePage.logout();
+    workspacePage.login(testConfig.testUser1, testConfig.testPassword1);
+
+    workspacePage.navigateToUserFolder(testUserName2);
+    shareModal.openDialogViaMoreOptions(folder, 'folder');
+    expect(shareModal.canChangeOwnership()).toBe(false);
+    shareModal.clickDone();
+  });
+
+
+  it("should fail to change ownership of a folder shared as readable with Everybody group", function () {
+    var folder = workspacePage.createFolder('Readable');
+
+    shareModal.shareResourceWithGroup(folder, 'folder', everybodyGroup, false, false);
+
+    workspacePage.logout();
+    workspacePage.login(testConfig.testUser2, testConfig.testPassword2);
+
+    workspacePage.navigateToUserFolder(testUserName1);
+    shareModal.openDialogViaMoreOptions(folder, 'folder');
+    expect(shareModal.canShare()).toBe(false);
+    shareModal.clickDone();
+  });
+
+
+  it("should fail to change ownership of a folder shared as writable with Everybody group", function () {
+    var folder = workspacePage.createFolder('Writable');
+
+    shareModal.shareResourceWithGroup(folder, 'folder', everybodyGroup, true, false); // TODO fails due to issue #273
+
+    workspacePage.logout();
+    workspacePage.login(testConfig.testUser1, testConfig.testPassword1);
+
+    workspacePage.navigateToUserFolder(testUserName2);
+    shareModal.openDialogViaMoreOptions(folder, 'folder');
+    expect(shareModal.canChangeOwnership()).toBe(false);
+    shareModal.clickDone();
+  });
 
 
 });
--- tests/modals/share-modal.js
+++ tests/modals/share-modal.js
@@ -6,14 +6,23 @@ var ShareModal = function () {
   var EC = protractor.ExpectedConditions;
 
   var shareModal = element(by.id('share-modal'));
-  var shareWithUserRow = shareModal.element(by.css('div > div > div.modal-body > div > div > div.col-sm-6.ng-scope > div:nth-child(5)'));
+  var shareModalBody = shareModal.element(by.css('div > div > div.modal-body '));
+  var shareWithGroupRow = shareModalBody.element(by.css('div > div > div.col-sm-6.ng-scope > div:nth-child(2)'));
+  var shareWithUserRow = shareModalBody.element(by.css('div > div > div.col-sm-6.ng-scope > div:nth-child(5)'));
   var shareModalUserName = shareWithUserRow.element(by.css('div.col-sm-7.typeaheadDropUp > input'));
-  var sharedModalPermissionsList = shareWithUserRow.element(by.css('div.btn-group.bootstrap-select.dropdown.col-sm-4.select-picker.ng-pristine.ng-untouched.ng-valid.ng-isolate-scope.ng-not-empty'));
-  var shareModalPermissions = sharedModalPermissionsList.element(by.css('button'));
-  var shareModalReadPermission = sharedModalPermissionsList.element(by.css('div > ul > li:nth-child(1) > a'));
-  var shareModalWritePermission = sharedModalPermissionsList.element(by.css('div > ul > li:nth-child(2) > a'));
-  var shareModalOwnerPermission = sharedModalPermissionsList.element(by.css('div > ul > li:nth-child(3) > a'));
+  var shareModalGroupName = shareWithGroupRow.element(by.css('div.col-sm-7 > input'));
+  var sharedModalUserPermissionsList = shareWithUserRow.element(by.css('div.btn-group.bootstrap-select.dropdown.col-sm-4.select-picker.ng-pristine.ng-untouched.ng-valid.ng-isolate-scope.ng-not-empty'));
+  var sharedModalGroupPermissionsList = shareWithGroupRow.element(by.css('div.btn-group.bootstrap-select.col-sm-4.select-picker.ng-pristine.ng-untouched.ng-valid.ng-isolate-scope.ng-not-empty'));
+  var shareModalUserPermissions = sharedModalUserPermissionsList.element(by.css('button'));
+  var shareModalGroupPermissions = sharedModalGroupPermissionsList.element(by.css('button'));
+  var shareModalUserReadPermission = sharedModalUserPermissionsList.element(by.css('div > ul > li:nth-child(1) > a'));
+  var shareModalGroupReadPermission = sharedModalGroupPermissionsList.element(by.css('div > ul > li:nth-child(1) > a'));
+  var shareModalUserWritePermission = sharedModalUserPermissionsList.element(by.css('div > ul > li:nth-child(2) > a'));
+  var shareModalGroupWritePermission = sharedModalGroupPermissionsList.element(by.css('div > ul > li:nth-child(2) > a'));
+  var shareModalUserOwnerPermission = sharedModalUserPermissionsList.element(by.css('div > ul > li:nth-child(3) > a'));
+  var shareModalGroupOwnerPermission = sharedModalGroupPermissionsList.element(by.css('div > ul > li:nth-child(3) > a'));
   var shareModalAddUserButton = shareWithUserRow.element(by.css('div.col-sm-1.pull-right > button'));
+  var shareModalAddGroupButton = shareWithGroupRow.element(by.css('div.col-sm-1.pull-right > button'));
   var shareModalDoneButton = shareModal.element(by.css('div > div > div.modal-footer.actions > div > button'));
 
 
@@ -21,8 +30,16 @@ var ShareModal = function () {
     return shareModal;
   };
 
+  this.createShareModalBody = function () {
+    return shareModalBody;
+  };
+
   this.createShareModalUserName = function () {
     return shareModalUserName;
+  };
+
+  this.createShareModalGroupName = function () {
+    return shareModalGroupName;
   };
 
   this.createShareModalDoneButton = function () {
@@ -33,20 +50,40 @@ var ShareModal = function () {
     return shareModalAddUserButton;
   };
 
-  this.createShareModalPermissions = function () {
-    return shareModalPermissions;
+  this.createShareModalAddGroupButton = function () {
+    return shareModalAddGroupButton;
   };
 
-  this.createShareModalReadPermission = function () {
-    return shareModalReadPermission;
+  this.createShareModalUserPermissions = function () {
+    return shareModalUserPermissions;
   };
 
-  this.createShareModalWritePermission = function () {
-    return shareModalWritePermission;
+  this.createShareModalGroupPermissions = function () {
+    return shareModalGroupPermissions;
   };
 
-  this.createShareModalOwnerPermission = function () {
-    return shareModalOwnerPermission;
+  this.createShareModalUserReadPermission = function () {
+    return shareModalUserReadPermission;
+  };
+
+  this.createShareModalUserWritePermission = function () {
+    return shareModalUserWritePermission;
+  };
+
+  this.createShareModalUserOwnerPermission = function () {
+    return shareModalUserOwnerPermission;
+  };
+
+  this.createShareModalGroupReadPermission = function () {
+    return shareModalGroupReadPermission;
+  };
+
+  this.createShareModalGroupWritePermission = function () {
+    return shareModalGroupWritePermission;
+  };
+
+  this.createShareModalGroupOwnerPermission = function () {
+    return shareModalGroupOwnerPermission;
   };
 
 
@@ -76,28 +113,34 @@ var ShareModal = function () {
 
   this.shareResource = function(name, type, username, canWrite, isOwner) {
     this.openDialogViaMoreOptions(name, type);
-    this.shareWith(username, canWrite, isOwner);
+    this.shareWithUser(username, canWrite, isOwner);
   };
 
 
-  this.shareWith = function (username, canWrite, isOwner) {
+  this.shareResourceWithGroup = function(name, type, groupname, canWrite, isOwner) {
+    this.openDialogViaMoreOptions(name, type);
+    this.shareWithGroup(groupname, canWrite, isOwner);
+  };
+
+
+  this.shareWithUser = function (username, canWrite, isOwner) {
     var usernameField = this.createShareModalUserName();
     browser.wait(EC.visibilityOf(usernameField));
     usernameField.sendKeys(username);
     browser.actions().sendKeys(protractor.Key.ENTER).perform();
 
     if (canWrite || isOwner) {
-      var permissionsList = this.createShareModalPermissions();
+      var permissionsList = this.createShareModalUserPermissions();
       browser.wait(EC.elementToBeClickable(permissionsList));
       permissionsList.click();
 
       if(canWrite) {
-        browser.wait(EC.elementToBeClickable(this.createShareModalWritePermission()));
-        this.createShareModalWritePermission().click();
+        browser.wait(EC.elementToBeClickable(this.createShareModalUserWritePermission()));
+        this.createShareModalUserWritePermission().click();
       }
       else if(isOwner) {
-        browser.wait(EC.elementToBeClickable(this.createShareModalOwnerPermission()));
-        this.createShareModalOwnerPermission().click();
+        browser.wait(EC.elementToBeClickable(this.createShareModalUserOwnerPermission()));
+        this.createShareModalUserOwnerPermission().click();
       }
     }
 
@@ -106,13 +149,67 @@ var ShareModal = function () {
       addButton.click();
     });
 
+    this.clickDone();
+  };
+
+
+  this.shareWithGroup = function(groupName, canWrite, isOwner) {
+    var groupnameField = this.createShareModalGroupName();
+    browser.wait(EC.visibilityOf(groupnameField));
+    groupnameField.sendKeys(groupName);
+    browser.actions().sendKeys(protractor.Key.ENTER).perform();
+
+    if (canWrite || isOwner) {
+      var permissionsList = this.createShareModalGroupPermissions();
+      browser.wait(EC.elementToBeClickable(permissionsList));
+      permissionsList.click();
+
+      if(canWrite) {
+        browser.wait(EC.elementToBeClickable(this.createShareModalGroupWritePermission()));
+        this.createShareModalGroupWritePermission().click();
+      }
+      else if(isOwner) {
+        browser.wait(EC.elementToBeClickable(this.createShareModalGroupOwnerPermission()));
+        this.createShareModalGroupOwnerPermission().click();
+      }
+    }
+
+    var addButton = this.createShareModalAddGroupButton();
+    browser.wait(EC.elementToBeClickable(addButton)).then(function () {
+      addButton.click();
+    });
+
+    this.clickDone();
+  };
+
+
+  // assuming the share modal is open, this checks whether this user can share the selected item
+  this.canShare = function() {
+    var usernameField = this.createShareModalUserName();
+    return usernameField.isPresent();
+  };
+
+
+  // assuming the share modal is open, this checks whether the current user can change ownership of the selected item
+  this.canChangeOwnership = function () {
+    var permissionsList = this.createShareModalUserPermissions();
+    if(!permissionsList.isPresent()) {
+      return false;
+    }
+    browser.wait(EC.elementToBeClickable(permissionsList));
+    permissionsList.click();
+    browser.wait(EC.elementToBeClickable(this.createShareModalUserReadPermission()));
+    var ownerPermission = this.createShareModalUserOwnerPermission();
+    return ownerPermission.isDisplayed();
+  };
+
+
+  this.clickDone = function() {
     var doneButton = this.createShareModalDoneButton();
     browser.wait(EC.visibilityOf(doneButton));
-    browser.actions().mouseMove(doneButton).perform();
-    browser.wait(EC.elementToBeClickable(doneButton)).then(function () {
-      doneButton.click();
-    });
-  };
+    browser.wait(EC.elementToBeClickable(doneButton));
+    doneButton.click();
+  }
 
 
 };
