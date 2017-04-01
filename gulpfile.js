162cb80f7a41a7bd79dd8d57b720e8ec3e6513c0
Debra Willrett <willrett@stanford.edu>
2017-03-31 22:30:40 -0700

    test-workspace

--- gulpfile.js
+++ gulpfile.js
@@ -172,13 +172,25 @@ gulp.task('e2e', ['test-env'], function () {
 gulp.task('test-workspace', ['test-env'], function () {
   return gulp.src([
     './tests/e2e/clean-up-spec.js',
-    './tests/e2e/delete-resource-spec.js',
-    './tests/e2e/folder-permissions-spec.js',
-    './tests/e2e/resource-permissions-spec.js',
     './tests/e2e/update-description-spec.js',
     './tests/e2e/update-name-spec.js',
     './tests/e2e/update-ownership-spec.js',
     './tests/e2e/update-permissions-spec.js'
+  ])
+      .pipe(protractor({
+        configFile: "protractor.config.js"
+      }))
+      .on('error', function (e) {
+        throw e
+      });
+});
+
+gulp.task('test-permissions', ['test-env'], function () {
+  return gulp.src([
+    './tests/e2e/clean-up-spec.js',
+    './tests/e2e/delete-resource-spec.js',
+    './tests/e2e/folder-permissions-spec.js',
+    './tests/e2e/resource-permissions-spec.js'
   ])
       .pipe(protractor({
         configFile: "protractor.config.js"
