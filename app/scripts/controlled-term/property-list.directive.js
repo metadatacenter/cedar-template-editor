d73feffd516b7a1765fcdec8aca5bfad2241000d
Debra Willrett <willrett@stanford.edu>
2017-04-12 12:05:20 -0700

    fix for multi-instance

--- app/scripts/controlled-term/property-list.directive.js
+++ app/scripts/controlled-term/property-list.directive.js
@@ -9,12 +9,8 @@ define([
   propertyListDirective.$inject = ["DataManipulationService","controlledTermDataService", "StringUtilsService"];
 
   /**
-   * display a list of field types for the node
+   * display a list of properties
    *
-   * @param DataManipulationService
-   * @param controlledTermDataService
-   * @param StringUtilsService
-   * @returns {{restrict: string, scope: {field: string}, templateUrl: string, link: Function}}
    */
   function propertyListDirective( DataManipulationService, controlledTermDataService, StringUtilsService) {
 
@@ -27,6 +23,41 @@ define([
       link    : function (scope, element, attrs) {
 
 
+        scope.property = null;
+
+        //// new property added
+        //scope.$on("property:propertyAdded", function () {
+        //  console.log('property:propertyAdded');
+        //  scope.property = scope.getProperties();
+        //});
+
+
+        scope.getProperties = function () {
+          //return DataManipulationService.getProperty(scope.field);
+        };
+
+        scope.deleteProperty = function () {
+         // DataManipulationService.deleteProperty(scope.field);
+        };
+
+        scope.getShortText = function (text, maxLength, finalString, emptyString) {
+          return StringUtilsService.getShortText(text, maxLength, finalString, emptyString);
+        };
+
+        scope.getShortId = function (uri, maxLength) {
+          return StringUtilsService.getShortId(uri, maxLength);
+        };
+
+        scope.getDescription = function () {
+        };
+
+        scope.getId = function () {
+        };
+
+        // table or popup view
+        scope.isTableView = function() {
+          return true;
+        }
 
 
       }
