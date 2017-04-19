62c1f1e11418ce9e251a9042f10455eb939c21b0
Debra Willrett <willrett@stanford.edu>
2017-04-19 15:28:55 -0700

    in process

--- app/scripts/controlled-term/controlled-term-search.directive.js
+++ app/scripts/controlled-term/controlled-term-search.directive.js
@@ -420,9 +420,14 @@ define([
 
               if (selection.details.type == 'OntologyClass') {
                 controlledTermService.loadTreeOfClass(selection.details, vm);
+
+                console.log(selection.details + ' ' + vm.stageValueConstraintAction);
+
+
                 handleClose(close);
               } else if (selection.details.type == 'Value') {
                 controlledTermService.loadTreeOfValue(selection.details, vm);
+
                 handleClose(close);
               }
             }
@@ -519,6 +524,7 @@ define([
 
             controlledTermDataService.getClassById(acronym, classId).then(function (response) {
               vm.classDetails = response;
+              console.log(vm.classDetails);
             });
           }
 
