ebff4e4913f5af8cfdd3038a16efa5d4d4509a38
Marcos Martinez <marcos.martinez.romero@gmail.com>
2017-04-21 10:37:31 -0700

    fix bug retrieving property children

--- app/scripts/controlled-term/cedar-child-tree.directive.js
+++ app/scripts/controlled-term/cedar-child-tree.directive.js
@@ -35,7 +35,14 @@ define([
         element.addClass('expanded');
         element.addClass('loaded');
 
-        var children = '<cedar-class-tree tree="' + children + '" term="term" level="' + (scope.level + 1) + '" is-selected-callback="isSelectedCallback" get-class-details-callback="getClassDetailsCallback"></cedar-class-tree>';
+        var children = '<cedar-class-tree ' +
+            'tree="' + children + '" ' +
+            'term="term" ' +
+            'level="' + (scope.level + 1) + '" ' +
+            'is-property-tree="isPropertyTree"' +
+            'is-selected-callback="isSelectedCallback" ' +
+            'get-class-details-callback="getClassDetailsCallback">' +
+            '</cedar-class-tree>';
         $compile(children)(scope, function (cloned, scope) {
           element.append(cloned);
         });
