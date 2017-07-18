48eecc407ee8171ff861e5aac32d321e1ebd8a88
Debra Willrett <willrett@stanford.edu>
2017-07-18 16:05:28 -0700

    flow modal metadata select and upload

--- app/scripts/form/flow-modal.directive.js
+++ app/scripts/form/flow-modal.directive.js
@@ -13,9 +13,9 @@ define([
 
         var directive = {
           bindToController: {
-            modalVisible  : '=',
-            files: '=',
-            instance: '='
+            modalVisible: '=',
+            files       : '=',
+            instance    : '='
           },
           controller      : flowModalController,
           controllerAs    : 'flowCntl',
@@ -26,25 +26,113 @@ define([
 
         flowModalController.$inject = [
           '$scope',
-          '$timeout'
+          '$rootScope',
+          '$timeout',
+          'QueryParamUtilsService',
+          'UISettingsService',
+          'UIMessageService',
+          'resourceService',
+          '$http'
         ];
 
-        function flowModalController($scope, $timeout) {
+        function flowModalController($scope, $rootScope, $timeout, QueryParamUtilsService, UISettingsService,
+                                     UIMessageService, resourceService, $http) {
           var vm = this;
           var vm = $scope;
 
-          $scope.init = function(flow) {
+          $scope.totalCount = -1;
+          $scope.instanceName;
+          $scope.offset = 0;
+          $scope.resources = [];
+          $scope.loadingInstances;
+          $scope.mode = 'ImmPort';
+
+          $scope.setMode = function (mode) {
+            $scope.mode = mode;
+            console.log($scope.mode);
           };
 
-          $scope.cancelAll = function(flow) {
+
+          // locations
+          $scope.selected = undefined;
+          $scope.getLocation = function (val) {
+            return $http.get('//maps.googleapis.com/maps/api/geocode/json', {
+              params: {
+                address: val,
+                sensor : false
+              }
+            }).then(function (response) {
+              return response.data.results.map(function (item) {
+                return item.formatted_address;
+              });
+
+            });
+          };
+
+
+          // instances
+          $scope.selectedInstance = undefined;
+          $scope.instances = function (term) {
+
+            var limit = UISettingsService.getRequestLimit();
+            var offset = 0;
+            var resourceTypes = ['instance'];
+            var sort = 'name';
+
+            return  resourceService.getSearchResourcesPromise(term,
+                {
+                  resourceTypes: resourceTypes,
+                  sort         : sort,
+                  limit        : limit,
+                  offset       : offset
+                },
+                function (response) {
+
+                  vm.resources = response.data.resources;
+                  return vm.resources.map(function (item) {
+                    return item.name;
+                  });
+                },
+                function (error) {
+                  UIMessageService.showBackendError('SERVER.SEARCH.error', error);
+                }
+            );
+          };
+
+
+          $scope.init = function (flow) {
+          };
+
+          $scope.insertItems = function (flow, name) {
+            $timeout(function () {
+
+              for (var i=0;i<vm.resources.length;i++) {
+                if (vm.resources[i].name === name) {
+                  // go get this document
+                  console.log(vm.resources[i]['@id']);
+                }
+              }
+
+              var debug = {hello: "world"};
+              var blob = new Blob([JSON.stringify(debug, null, 2)], {type: 'application/json'});
+              blob.name = name + '.json';
+              flow.addFile(blob);
+            }, 0);
+          };
+
+          $scope.startUpload = function (flow) {
+            flow.upload();
+          };
+
+          $scope.cancelAll = function (flow) {
             flow.cancel();
           };
 
-          $scope.pauseAll = function(flow) {
+          $scope.pauseAll = function (flow) {
             flow.pause();
           };
 
-          $scope.resumeAll = function(flow) {
+          $scope.resumeAll = function (flow) {
             flow.resume();
           };
 
@@ -60,18 +148,20 @@ define([
             console.log('flow::progress');
           });
 
-          $scope.flowProgress = function(flow) {
-            console.log('flow::progress ');console.log(flow);
+          $scope.flowProgress = function (flow) {
+            console.log('flow::progress ');
+            console.log(flow);
           };
 
-          $scope.flowFileProgress = function(flow, file) {
-            console.log('flow::fileProgress ');console.log(file);
+          $scope.flowFileProgress = function (flow, file) {
+            console.log('flow::fileProgress ');
+            console.log(file);
           };
 
           //from the 'UploadCtrl as uploadctrl' controller, which has scope on the whole thing
           $scope.$on('flow::complete', function (event, $flow) {
             console.log('flow::complete');
-            $timeout(function(){
+            $timeout(function () {
               console.log('cancel');
               $flow.cancel();
             }, 5000);
