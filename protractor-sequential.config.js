156cce02ae4177ab21135f46ce515b3b2ed23075
Debra Willrett <willrett@stanford.edu>
2017-07-31 12:17:28 -0700

    fixed screen size

--- protractor-sequential.config.js
+++ protractor-sequential.config.js
@@ -9,7 +9,7 @@ exports.config = {
   capabilities: {
     browserName   : 'chrome',
     chromeOptions: {
-      args: ["--headless", 'no-sandbox', "--disable-gpu", "--window-size=800x600"]
+      args: ["--headless", 'no-sandbox', "--disable-gpu", "--window-size=1920x1200"]
     },
     shardTestFiles: false,
     maxInstances  : 1
