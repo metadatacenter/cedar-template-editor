912f8812a6717422719da6e2aa23cd33b46a7704
Debra Willrett <willrett@stanford.edu>
2017-10-03 21:15:12 -0700

    static field test on again

--- tests/e2e/metadata-creator-spec.js
+++ tests/e2e/metadata-creator-spec.js
@@ -214,15 +214,15 @@ describe('metadata-creator', function () {
         expect(value).toBe(2);
       });
 
-      // var firstField = fields.first();
-      // expect(firstField.isPresent()).toBe(true);
+      var firstField = fields.first();
+      expect(firstField.isPresent()).toBe(true);
       // browser.actions().mouseMove(firstField).perform();
       // browser.wait(EC.elementToBeClickable(firstField));
       // firstField.click();
       // expect(templatePage.createImage().isPresent()).toBe(true);
 
-      // var lastField = fields.last();
-      // expect(lastField.isPresent()).toBe(true);
+      var lastField = fields.last();
+      expect(lastField.isPresent()).toBe(true);
       // browser.actions().mouseMove(lastField).perform();
       // browser.wait(EC.elementToBeClickable(lastField));
       // lastField.click();
