6ba146aa4769324a52930fd727e804392303ba5e
Debra Willrett <willrett@stanford.edu>
2018-02-22 10:03:49 -0800

    with spreadsheets

--- tests/e2e/delete-resource-spec.js
+++ tests/e2e/delete-resource-spec.js
@@ -150,6 +150,74 @@ describe('delete-resource', function () {
 
       toastyModal.isSuccess();
     });
+
+    it("should fail to change permissions of a folder shared as readable with current user", function () {
+      console.log('update-permissions should fail to change permissions of a folder shared as readable with current user');
+      workspacePage.login(testConfig.testUser1, testConfig.testPassword1);
+
+      var folder = workspacePage.createFolder('Readable');
+      resources.push(createResource(folder, 'folder', testConfig.testUser1, testConfig.testPassword1));
+
+      shareModal.shareResource(folder, 'folder', testConfig.testUserName2, false, false);
+      workspacePage.clearSearch();
+
+      workspacePage.login(testConfig.testUser2, testConfig.testPassword2);
+      workspacePage.navigateToUserFolder(testConfig.testUserName1);
+      shareModal.shareDisabledViaRightClick(folder, 'folder');
+
+    });
+
+
+
+    it("should be able to change permissions of a folder shared as writable with Everybody", function () {
+      console.log('update-permissions should be able to change permissions of a folder shared as writable with Everybody');
+      workspacePage.login(testConfig.testUser2, testConfig.testPassword2);
+
+      var folder = workspacePage.createFolder('Writable');
+      resources.push(createResource(folder, 'folder', testConfig.testUser2, testConfig.testPassword2));
+
+      shareModal.shareResourceWithGroup(folder, 'folder', testConfig.everybodyGroup, true, false);
+      workspacePage.clearSearch();
+
+      workspacePage.login(testConfig.testUser1, testConfig.testPassword1);
+      workspacePage.navigateToUserFolder(testConfig.testUserName2);
+      shareModal.shareResourceWithGroup(folder, 'folder', testConfig.everybodyGroup, false, false);
+
+    });
+
+
+    it("should be able to change permissions of a folder shared as writable with current user", function () {
+      console.log('update-permissions should be able to change permissions of a folder shared as writable with current user');
+      workspacePage.login(testConfig.testUser2, testConfig.testPassword2);
+
+      var folder = workspacePage.createFolder('Writable');
+      resources.push(createResource(folder, 'folder', testConfig.testUser2, testConfig.testPassword2));
+
+      shareModal.shareResource(folder, 'folder', testConfig.testUserName1, true, false);
+      workspacePage.clearSearch();
+
+      workspacePage.login(testConfig.testUser1, testConfig.testPassword1);
+      workspacePage.navigateToUserFolder(testConfig.testUserName2);
+      shareModal.shareResourceWithGroup(folder, 'folder', testConfig.everybodyGroup, false, false);
+
+    });
+
+
+    it("should fail to change permissions of a folder shared as readable with Everybody group", function () {
+      console.log('update-permissions should fail to change permissions of a folder shared as readable with Everybody group');
+      workspacePage.login(testConfig.testUser1, testConfig.testPassword1);
+
+      var folder = workspacePage.createFolder('Readable');
+      resources.push(createResource(folder, 'folder', testConfig.testUser1, testConfig.testPassword1));
+
+      shareModal.shareResourceWithGroup(folder, 'folder', testConfig.everybodyGroup, false, false);
+      workspacePage.clearSearch();
+
+      workspacePage.login(testConfig.testUser2, testConfig.testPassword2);
+      workspacePage.navigateToUserFolder(testConfig.testUserName1);
+      shareModal.shareDisabledViaRightClick(folder, 'folder');
+
+    });
   });
 
   describe('remove created resources', function () {
