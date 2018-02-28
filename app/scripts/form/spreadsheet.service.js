fa3f49ec6d7b9c181041c652c1b2c3139ba80857
Debra Willrett <willrett@stanford.edu>
2018-02-28 14:16:55 -0800

    json model changes for a-v fields

--- app/scripts/form/spreadsheet.service.js
+++ app/scripts/form/spreadsheet.service.js
@@ -400,7 +400,6 @@ define([
               colHeaders.push('<span  title="' + description + '">' + title + ' </span>');
             }
           }
-          console.log('colHeaders',colHeaders)
           return colHeaders;
         };
 
