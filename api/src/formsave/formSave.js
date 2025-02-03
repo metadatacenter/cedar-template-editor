884c4bd5bc95ae7f3c69a17a0660c213cce41470
elario <elario@carrerasresearch.org>
2025-02-03 09:36:33 +0000

    Update template

--- api/src/formsave/formSave.js
+++ api/src/formsave/formSave.js
@@ -28,7 +28,7 @@ const writeFileAsync = promisify(fs.writeFile);
 function findProjectAndInvestigator(jsonData) {
 
     const projectName = jsonData['Project Name']?.['@value'] || '';
-    const principalInvestigator = jsonData['IJC_PI_contact_info']?.Name?.['@value'] || '';
+    const principalInvestigator = jsonData['PI']?.Name?.['@value'] || '';
    
     return `${projectName}-${principalInvestigator}`;
 }
